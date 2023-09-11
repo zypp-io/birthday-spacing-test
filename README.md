278f1dc334ebd2fb94e8c2f9debf6e36235ee358
Erfan Nariman <erfannariman@gmail.com>
2023-09-12 00:18:21 +0200

    Finish first version

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# birthday-spacing-test
-Birthday spacing test
+# Zypper random generator
+
+By [Zypper](https://www.zypper.app) gebruiken we de [`numpy`](https://numpy.org/doc/stable/reference/random/generator.html) random generator om de start euro te bepalen. De random generator algoritme die wordt gebruikt is [`PCG-64`](https://numpy.org/doc/stable/reference/random/bit_generators/pcg64.html#numpy.random.PCG64) and is een moderne en statistische goed algoritme.
+
+In deze repo bewijzen we door middel van de [`Birthday Spacing Test`](https://en.wikipedia.org/wiki/Diehard_tests) de randomness van `numpy` random generator die wordt gebruikt binnen Zypper.
+
+<img src="dist.png" alt="dist image" width="800"/>
+<br><br>
+
+# Quickstart (linux / MacOS)
+
+1. Maak een virtual env aan:
+
+```
+python -m venv venv && source venv/bin/activate
+```
+
+2. Installeer de nodige packages
+
+```
+pip install -r requirements.txt
+```
+
+3. Run de test
+
+```python
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+Created by Zypp, 12-09-2023
+"""
+
+import numpy as np
+import pandas as pd
+from scipy.stats import expon, kstest
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_results(spacings: np.array, params):
+    sns.histplot(spacings, kde=True, stat='density', bins=30, label='Empirical Distribution')
+    x = np.linspace(min(spacings), max(spacings), 100)
+    pdf = expon.pdf(x, *params)
+    plt.plot(x, pdf, label='Theoretical Exponential Distribution', color='red')
+    plt.title('Distribution of Spacings')
+    plt.xlabel('Spacing')
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
+
+
+def birthday_spacing_test(n_numbers: int, range_interval: float):
+    
+    # Generate random uniform point
+    random_points = np.random.uniform(low=0.0, high=range_interval, size=n_numbers)
+
+    # Calculate the spacings between the random points
+    random_points = np.sort(random_points)
+    spacings = np.diff(random_points)
+
+    # Conduct Goodness-of-Fit Test (Kolmogorov-Smirnov Test in this case)
+    # Fit the spacings to an exponential distribution to get the parameters
+    params = expon.fit(spacings)
+
+    # Conduct the Kolmogorov-Smirnov test to compare the empirical and theoretical distributions
+    ks_statistic, p_value = kstest(spacings, 'expon', args=params)
+
+    plot_results(spacings, params)
+
+    print("KS Statistic:", ks_statistic)
+    print("P-value:", p_value)
+
+    if p_value > 0.05:
+        print("We fail to reject the null hypothesis, this means that the random generator produces uniformly distributed spacings")
+    else:
+        print("The random generator is not random")
+
+
+if __name__ == "__main__":
+    birthday_spacing_test(n_numbers=1_000_000, range_interval=1_000_000)
